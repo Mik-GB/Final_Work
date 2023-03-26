00b0ff500da014a20d18406f43539fd5bfaf2a5c
Pronin Mike <mr.mi@mail.ru>
2023-03-27 01:16:44 +0400

    2 версия написания программы

--- Program.cs
+++ Program.cs
@@ -7,20 +7,20 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
-string[] arrayInit = new string[5] {"723", "1223", "hello", "world", "res"};
-string[] arrayFinite = new string[arrayInit.Length];
-void SecondArrayWithIF(string[] arrayInit, string[] arrayFinite)
-{
-    int count = 0;
-    for (int i = 0; i < arrayInit.Length; i++)
-    {
-    if(arrayInit[i].Length <= 3)
-        {
-        arrayFinite[count] = arrayInit[i];
-        count++;
-        }
-    }
-}
+// string[] arrayInit = new string[5] { "723", "1223", "hello", "world", "res" };
+// string[] arrayFinite = new string[arrayInit.Length];
+// void ResultArray(string[] arrayInit, string[] arrayFinite)
+// {
+//     int count = 0;
+//     for (int i = 0; i < arrayInit.Length; i++)
+//     {
+//         if (arrayInit[i].Length <= 3)
+//         {
+//             arrayFinite[count] = arrayInit[i];
+//             count++;
+//         }
+//     }
+// }
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -29,8 +29,31 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
-Console.WriteLine("Исходный массив:");
-PrintArray(arrayInit);
-SecondArrayWithIF(arrayInit, arrayFinite);
+// Console.WriteLine("Исходный массив:");
+// PrintArray(arrayInit);
+// ResultArray(arrayInit, arrayFinite);
+// Console.WriteLine("Массив из строк, длина которых меньше либо равна 3 символам:");
+// PrintArray(arrayFinite);
+
+
+Console.Write("Введите количество элементов для ввода ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayStrings = new string[size];
+int symbol = 3;
+int posit = 0;
+
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    if (element.Length <= symbol)
+    {
+        arrayStrings[posit] = element;
+        posit++;
+    }
+}
+
+Console.WriteLine();
 Console.WriteLine("Массив из строк, длина которых меньше либо равна 3 символам:");
-PrintArray(arrayFinite);
+PrintArray(arrayStrings);
